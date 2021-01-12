c802808bb4b247355377a6571b28b7689ab8c08b
Luc Nikiema <35904327+nikiluc@users.noreply.github.com>
2021-01-12 13:41:41 -0500

    Add files via upload

---
+++ src/App.js
@@ -0,0 +1,63 @@
+import React, { useState, useEffect } from 'react';
+import alanBtn from '@alan-ai/alan-sdk-web';
+//import { common } from '@material-ui/core/colors';
+import wordsToNumbers from 'words-to-numbers';
+
+import NewsCards from './components/NewsCards/NewsCards';
+//import classes from '*.module.css';
+
+import useStyles from './styles.js';
+
+const alanKey = '3ac0a178b70829935d9ec940df3f5af82e956eca572e1d8b807a3e2338fdd0dc/stage';
+
+const App = () => {
+
+    const [newsArticles, setNewsArticles] = useState([]);
+    const [activeArticle, setActiveArticle] = useState(-1);
+    
+    const classes = useStyles();
+
+    useEffect(() => {
+        alanBtn({
+            key: alanKey,
+            onCommand: ({command, articles, number}) => {
+                if (command === 'newHeadlines'){
+                    console.log(articles);
+                    setNewsArticles(articles);
+                    setActiveArticle(-1);
+                }
+                else if (command === 'highlight'){
+                    setActiveArticle((prevActiveArticle) => prevActiveArticle + 1);
+                }
+                else if (command === 'open') {
+                    const parsedNumber = number.length > 2 ? wordsToNumbers((number), { fuzzy: true }) : number;
+                    const article = articles[parsedNumber - 1];
+
+                    console.log(articles)
+          
+                    if (parsedNumber > articles.length) {
+                        alanBtn().playText('Please try that again...');
+                    } 
+                    else if (article) {
+                        window.open(article.url, '_blank');
+                        alanBtn().playText('Opening...');
+                    } 
+                    else {
+                        alanBtn().playText('Please try that again...');
+                    }
+                }
+            }
+        })
+    }, [])
+
+    return (
+        <div>
+            <div className={classes.logoContainer}>
+                <img src= 'https://alan.app/voice/images/previews/preview.jpg' className={classes.alanLogo} alt="alan logo"/>
+            </div>
+            <NewsCards articles={newsArticles} activeArticle={activeArticle} />
+        </div>
+    )
+}
+
+export default App;
